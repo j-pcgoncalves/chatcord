3d90707df82a8eaa89ab0ffa73a619f4b1fefae2
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2023-05-29 19:39:58 +0100

    create formatMessage and locationGenerator

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require("path");
 const http = require("http");
 const express = require("express");
 const socketio = require("socket.io");
+const { formatMessage, locationGenerator } = require("./utils/messages");
 
 const app = express();
 const server = http.createServer(app);
---
+++ utils/messages.js
@@ -0,0 +1,22 @@
+const moment = require("moment");
+
+function formatMessage(username, text) {
+    return {
+        username,
+        text,
+        time: moment().format("h:mm a")
+    };
+}
+
+function locationGenerator(username, coords) {
+    return {
+        lat: coords.lat,
+        lon: coords.lon,
+        time: moment().format("h:mm a")
+    }
+}
+
+module.exports = {
+    locationGenerator,
+    formatMessage
+};
