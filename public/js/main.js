8b7ba44b260492a7ed4dbec11aa21dc83a75eb22
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2023-05-29 21:04:48 +0100

    add functionality to chat room

---
+++ public/js/main.js
@@ -0,0 +1,134 @@
+const chatForm = document.getElementById("chat-form");
+const chatMessages = document.querySelector(".chat-messages");
+const roomName = document.getElementById("room-name");
+const userList = document.getElementById("users");
+const locationBtn = document.getElementById("location-btn");
+
+// Get username and room from URL
+const { username, room } = Qs.parse(location.search, {
+    ignoreQueryPrefix: true,
+});
+
+const socket = io();
+
+// Join chatroom
+socket.emit("joinRoom", { username, room });
+
+// Get room and users
+socket.on("roomUsers", ({ room, users }) => {
+    outputRoomName(room);
+    outputUsers(users);
+});
+
+// Message from server
+socket.on("message", message => {
+    outputMessage(message);
+
+    // Scroll down
+    chatMessages.scrollTop = chatMessages.scrollHeight;
+});
+
+// Message submit
+chatForm.addEventListener("submit", e => {
+    e.preventDefault();
+
+    // Get message text
+    let msg = e.target.elements.msg.value;
+
+    msg = msg.trim();
+
+    if (!msg) {
+        return false;
+    }
+
+    // Emit message to server
+    socket.emit("chatMessage", msg);
+
+    // Clear input
+    e.target.elements.msg.value = "";
+    e.target.elements.msg.focus();
+});
+
+// Listen for send location
+locationBtn.addEventListener("submit", e => {
+    if (!navigator.geolocation) {
+        return alert("Geolocation is not supported in your browser!");
+    } else {
+        navigator.geolocation.getCurrentPosition( function(position) {
+            socket.emit("sendLocationInfos", {
+                username: username,
+                lat: position.coords.latitude,
+                lon: position.coords.longitude
+            })
+        }, function() {
+            alert("Unable to get location.")
+        })
+    };
+
+    e.preventDefault();
+});
+
+// Location message
+socket.on("sendLocationMessage", message => {
+    outputLocationMessage(message);
+    chatMessages.scrollTop = chatMessages.scrollHeight;
+})
+
+// Output message to DOM
+function outputMessage(message) {
+    const div = document.createElement("div");
+    div.classList.add("message");
+    const p = document.createElement("p");
+    p.classList.add("meta");
+    p.innerText = message.username;
+    p.innerHTML += `<span>${message.time}</span>`;
+    div.appendChild(p);
+    const para = document.createElement("p");
+    para.classList.add("text");
+    para.innerText = message.text;
+    div.appendChild(para);
+    document.querySelector(".chat-messages").appendChild(div);
+}
+
+// Location message to DOM
+function outputLocationMessage(message) {
+    const div = document.createElement("div");
+    div.classList.add("message");
+    const p = document.createElement("p");
+    p.classList.add("meta");
+    p.innerText = message.username;
+    p.innerHTML = `<span>${message.time}</span>`;
+    div.appendChild(p);
+
+    const a = document.createElement("a");
+    a.innerText = "Here is my location!";
+    a.setAttribute("href", `https://google.com/maps?q=${message.lat},${message.lon}`);
+    a.setAttribute("target", "_blank");
+    div.appendChild(a);
+    document.querySelector(".chat-messages").appendChild(div);
+}
+
+// Add room name to DOM
+function outputRoomName(room) {
+    roomName.innerText = room;
+}
+
+// Add users to DOM
+function outputUsers(users) {
+    userList.innerHTML = "";
+    users.forEach(user => {
+        const li = document.createElement("li");
+        li.innerText = user.username;
+        userList.appendChild(li);
+    });
+}
+
+// Prompt the user before leaving the chat room
+document.getElementById("leave-btn").addEventListener("click", () => {
+    const leaveRoom = confirm("Are you sure you want to leave the chatroom?");
+
+    if (leaveRoom) {
+        window.location = "../index.html";
+    } else {
+    }
+});
